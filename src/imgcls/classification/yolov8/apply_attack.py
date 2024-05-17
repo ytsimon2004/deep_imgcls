057250f892801e8b792421435ef97751afe86f8c
Yu-Ting Wei <ytsimon2004@gmail.com>
2024-05-17 16:04:16 +0200

    clean

--- src/imgcls/classification/yolov8/apply_attack.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-f = '/opt/homebrew/runs/detect/train2/weights/best.pt'
-
-test = '/Users/yuting/.cache/comvis/imgcls/test/img_png'
-
-model = YOLO(f)
-model.predict(test, save=True)
